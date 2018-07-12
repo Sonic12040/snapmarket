3c1883cadee6db1a8f1cb3a7a46cd286f1c11e29
melissaprax <melissaprax@gmail.com>
2018-07-11 20:58:28 -0500

    component folder and components created + css

---
+++ client/src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Main = () => 
+    <div>
+        <div>This is to hold some stuffs</div>
+        <div></div>
+    </div>
+
+
+
+export default Main;
