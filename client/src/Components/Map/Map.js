5a2da085d360bf62a318a711f30ddc7e62e8fce1
Ryan Cybul <ryan.cybul@gmail.com>
2018-07-14 17:20:38 -0500

    added .env file, removed map script tag from index.html, made map component more legit

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -7,30 +7,36 @@ const {
   InfoWindow,
 } = require("react-google-maps");
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const mapURL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+
 class Map extends Component {
    render() {
-   const GoogleMapExample = withGoogleMap(props => (
+   const MapWithAMarker = withScriptjs(withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-        defaultZoom = { 13 }
+        defaultCenter = { { lat:38.8283,lng:-98.5795 } }
+        defaultZoom = { 4.6 }
       >
 
       <Marker
-      position={{ lat: 40.756795, lng: -73.954298  }}
+      position={{ lat:38.8283,lng:-98.5795  }}
       onClick={props.onToggleOpen}
       />
 
       {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+      <p>Test</p>
       </InfoWindow>}
       </GoogleMap>
-   ));
+   )));
 
    return(
       <div>
-        <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
+        <MapWithAMarker
+          googleMapURL= {mapURL}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+      />
       </div>
    );
    }
