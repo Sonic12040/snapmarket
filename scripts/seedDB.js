a85596b5e1d7a624d7e84367c3012fb010b19638
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-07-14 09:40:37 -0500

    Two More Seeds. 2018-07-14 09:40

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -45,7 +45,39 @@ const marketSeed = [
         },
         Items: ['Vegetables','Jams','Fruits']
     },
-]
+    {
+        MarketName: 'Arkansas Local Food Network Online Farmers Market',
+        Address: '509 Scott St. Little Rock, Arkansas',
+        Coordinates: {
+            Latitude: '34.743076',
+            Longitude: '-92.269599'
+        },
+        ZipCode: '72201',
+        Benefits: {
+            Wic: false,
+            Wiccash: false,
+            Snap: false,
+            SFMNP: false
+        },
+        Items: ['Organic','Baked Goods','Cheese','Crafts','Flowers','Eggs','Herbs','Vegetables','Honey','Jams','Meat','Nuts','Plants','Poultry','Prepared','Soap','Trees','Coffee','Beans','Fruits','Juices','Mushrooms','Pet Food','Wild Harvested']
+    },
+    {
+        MarketName: 'Berryessa Farmers\' Market',
+        Address: '1376 Piedmont Road, San Jose, California',
+        Coordinates: {
+            Latitude: '37.40204',
+            Longitude: '-121.847368'
+        },
+        ZipCode: '95132',
+        Benefits: {
+            Wic: true,
+            Wiccash: false,
+            Snap: true,
+            SFMNP: false
+        },
+        Items: ['Organic','Baked Goods','Flowers','Seafood','Vegetables','Honey','Plants','Poultry','Prepared','Trees','Fruits','Mushrooms']
+    },
+];
 
 db.Market
     .remove({})
