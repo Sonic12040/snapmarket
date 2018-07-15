f02af11d9c18ebbc50612b546799f081c67cbfcf
Ryan Cybul <ryan.cybul@gmail.com>
2018-07-14 20:23:05 -0500

    functional info window

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+const { compose, withStateHandlers } = require("recompose");
 const {
   withScriptjs,
   withGoogleMap,
@@ -8,33 +9,45 @@ const {
 } = require("react-google-maps");
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 const mapURL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+console.log(mapURL);
 
 class Map extends Component {
    render() {
-   const MapWithAMarker = withScriptjs(withGoogleMap(props => (
+   const MapWithAMarker = compose(
+    withStateHandlers(() => ({
+      isOpen: false,
+    }), {
+      onToggleOpen: ({ isOpen }) => () => ({
+        isOpen: !isOpen,
+      })
+    }),
+    withScriptjs,
+    withGoogleMap
+  )(props => 
       <GoogleMap
         defaultCenter = { { lat:38.8283,lng:-98.5795 } }
         defaultZoom = { 4.6 }
       >
-
-      <Marker
-      position={{ lat:38.8283,lng:-98.5795  }}
-      onClick={props.onToggleOpen}
-      />
-
-      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-      <p>Test</p>
-      </InfoWindow>}
+        {/* //Create loop to place all markers from database */}
+        <Marker
+        position={{ lat:38.8283,lng:-98.5795  }}
+        onClick={props.onToggleOpen}
+        >
+        {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+          <h1>Oh hai!</h1>
+        </InfoWindow>}
+        </Marker>
       </GoogleMap>
-   )));
+   );
 
    return(
       <div>
         <MapWithAMarker
           googleMapURL= {mapURL}
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
+          containerElement={<div style={{ height: `800px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
       />
       </div>
