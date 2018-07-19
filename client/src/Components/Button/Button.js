5137146496452d6c09aff02a29377d3502da6788
melissaprax <melissaprax@gmail.com>
2018-07-18 19:23:00 -0500

    css button refinements

--- client/src/Components/Button/Button.js
+++ client/src/Components/Button/Button.js
@@ -3,54 +3,18 @@ import React from "react";
 
 const Button = (props) => 
     <div>
-        <div className="button-1">{props.name}</div>
+        <div className="button-1">
+        <div className="eff-1"></div>
+        {props.name}
+            </div>
     </div>
 
 
 
 
-// const waysToPay = ({Payments}) => (
-//     <div>
-//       {Payments.map(Payment => (
-//         <div className="Payments" key={Payment.call}>{Payment.call}</div>
-//       ))}
-//     </div>
 
 
 
-//   ); 
-
-
-
-// const Button = (Payment) => {
-//     return (
-//       <div>
-//         {Payments.map(Payment => {
-//           return <div>{Payment}</div>
-//         })}
-//       </div>
-//     )
-//   }
-
-// .map( ( {name} ) => {
-//     return <Button key={Payments.name} />
-// })
-
-
-// .map((Payments, index) => {
-//     return (
-//         <Button key={`${Payments.name}`}>
-//             {Payments.name}
-//         </Button>
-//     );
-// })
-
-
-
-
-
-
-  
 
 
 export default Button;
