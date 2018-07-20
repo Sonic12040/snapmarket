ea8d1b100dcad5cfc3e45834f318e8900d8913ac
melissaprax <melissaprax@gmail.com>
2018-07-19 21:30:32 -0500

    button css transition

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,11 +3,11 @@ import React from "react";
 
 const Header = () => 
 
-    <div className="height30vh t-a-c bg-E5DCD1 mb-2em">
-            <div className="fs-fredericka color-2D2D2D fs-5em">CarrotSnap
+    <div className=" t-a-c bg-E5DCD1 mb-2em pad-2em">
+            <div className="fs-fredericka color-2D2D2D fs-5em l-h1">CarrotSnap
             {/* insert carrot logo here */}
             </div>
-                <p className="fs-didact color-2D2D2D">find farmer's markets with benefits near you</p>
+                <p className="fs-didact color-2D2D2D l-h1 fs-1-25rem">find farmer's markets with benefits near you</p>
     </div>
 
 
