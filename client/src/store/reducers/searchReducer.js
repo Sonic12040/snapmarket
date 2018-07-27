05ea044b474f9fc393d03bc50dd5095f7dfbdb23
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-07-27 14:18:43 -0500

    Further improvements to the filterMarket function in refactoring. However, testing another seed shows a problem with the data passed through. The data from one end is not === to the data from the other end since they are formatted differently.

--- client/src/store/reducers/searchReducer.js
+++ client/src/store/reducers/searchReducer.js
@@ -6,7 +6,7 @@ const convertBenefitsToArray = (market) => {
         benefitsArray.push("WIC");
     };
     if (market.WicCash) {
-        benefitsArray.push("WICCash");
+        benefitsArray.push("WIC CASH");
     };
     if (market.Snap) {
         benefitsArray.push("SNAP");
@@ -21,22 +21,18 @@ const convertBenefitsToArray = (market) => {
 
 
 const filterMarket = (userButton, filter) => {
-    const testArrayResult = []; //holds the keys from userButton where the value is true
     for (const key in userButton) {
+        console.log('key', key);
+        console.log('filter', filter);
         if (userButton[key] === true) {
-            testArrayResult.push(key)
+            if (filter.includes(key) {
+                console.log('returning true');
+                return true;
+            }
         }
     }
-    console.log('Test Array Result', testArrayResult); //console log check of the result
-    filter.forEach(benefit => { //filter is an array of the benefits a market accepts
-        if (testArrayResult.includes(benefit)) {
-            console.log('Benefit passed: ', benefit);
-            return true; //only one benefit has to match
-        } else {
-            console.log('checkFail: ', false);
-        }
-        return false;
-    });
+    console.log('returning false');
+    return false;
 }
 
 const initialState = {query: "", results: [], benefitsArray: []};
