d0a109ca12c5182a730407983c161ae8369bb5b4
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-07-27 11:27:55 -0500

    Logic fixes in Search Reducer for an array comparison.

--- client/src/store/reducers/searchReducer.js
+++ client/src/store/reducers/searchReducer.js
@@ -21,11 +21,21 @@ const convertBenefitsToArray = (market) => {
 
 
 const filterMarket = (userButton, filter) => {
-    const array = Object.entries(userButton).map((key) => key[1]);
-    // const array = Object.entries(userButton).map((key) => key[1] >-1); // for testing
-return filter.some(key => key === array);
-    // return filter.some(key => key === array); // original line, always returns false
-        // return filter.every(key => key !== array); // test line, always returns true
+    const nestedArray = Object.entries(userButton);
+    const testArrayResult = [];
+    nestedArray.forEach(subArray => {
+        if (subArray[1] === true) {
+            testArrayResult.push(subArray[0]);
+        }
+    });
+    console.log('Test Array Result', testArrayResult);
+    filter.forEach(benefit => {
+        if (testArrayResult.includes(benefit)) {
+            return true;
+        } else {
+            console.log('checkFail', false);
+        }
+    });
 }
 
 const initialState = {query: "", results: [], benefitsArray: []};
